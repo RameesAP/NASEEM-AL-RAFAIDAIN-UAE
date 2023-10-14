9c2c85bfd34490d4b1165539cfb79d9222939436
RameesAP <rameeswork@gmail.com>
2023-10-14 22:29:31 +0530

    sec

---
+++ src/common/constants/home.js
@@ -0,0 +1,5 @@
+import Background from "@/assets/background.svg"
+
+export default{
+    Background,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import home from "@/common/constants/home";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="main flex justify-center ">
+      <div className="2xl:container w-full h-full flex:col justify-center ">
+        <div className="w-full h-[600px] mb-8  bg-cover bg-center bg-no-repeat relative ">
+          <div>
+            <Image className="w-full absolute" src={home.Background} alt="bg" />
+          </div>
+          <div className="absolute top-52 left-[550px] font-[700] text-[65px] text-[#32286E] ">WELCOME TO</div>
+        </div>
+      </div>
+    </div>
+  );
+}
