0e97865ae998594479b60b95f3717a095b9f6a7d
RameesAP <rameeswork@gmail.com>
2023-10-15 13:26:37 +0530

    arab

--- src/common/constants/home.js
+++ src/common/constants/home.js
@@ -1,5 +1,7 @@
 import Background from "@/assets/back.svg"
+import Arab from "@/assets/arab.png"
 
 export default{
     Background,
+    Arab
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,12 @@ export default function Home() {
           <div>
             <Image className="w-full absolute" src={home.Background} alt="bg" />
           </div>
-          <div className="absolute top-56 flex items-center justify-center w-full h-28">
-            <div className="font-[700] text-[65px] text-[#32286E] ">
-              WELCOME TO
+          <div className="absolute top-56 flex items-center justify-center w-full h-fit">
+            <div className="flex flex-col items-center justify-center ">
+              <div className="font-[700] text-[65px] text-[#32286E] ">
+                WELCOME TO
+              </div>
+              <div className="font-[700] text-[140px] text-red-500">نسيم الرافدين</div>
             </div>
           </div>
         </div>
