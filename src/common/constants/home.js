dc4b8ecc76e2f25cc44733255f4ea098dff2c764
RameesAP <rameeswork@gmail.com>
2023-10-15 21:47:18 +0530

    1st page completed

--- src/common/constants/home.js
+++ src/common/constants/home.js
@@ -1,8 +1,10 @@
 import Background from "@/assets/back.svg"
 import logo from "@/assets/mainpglogo.png"
+import tireandbat from "@/assets/tb.png"
 
 
 export default{
     Background,
-    logo
+    logo,
+    tireandbat
 };
